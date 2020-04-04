0b16382a22b432aef5114ba1bd734e50745a9213
Abhay Kumar Verma <56693429+darkRaspberry@users.noreply.github.com>
2020-04-04 21:28:45 +0530

    Backend added..
    By firebase.
    Google firebase Added for All the backend features.
    YUUUUUUUUUUUUUUUUUUUUUUUPPPPP.

---
+++ wp-content/assets/js/sdsdf.js
@@ -0,0 +1,33 @@
+/*Testing evolve js*/
+
+    /* only execute this script when the document is ready */
+$(document).ready(function(){
+    /* function called when you click of the button */
+    $(".navbutt").click(function(){
+        /* this if else to change the text in the button*/
+        if($(".navbutt").text() == "☰"){
+         $(".navbutt").text("🞬");
+        }else{
+          $(".navbutt").text("☰");
+        }
+      /* this function toggle the visibility of our "li" elements */
+      $("li").toggle("slow");
+    });
+  });
+
+
+
+  /* only execute this script when the document is ready */
+$(document).ready(function(){
+  /* function called when you click of the button */
+  $(".navbutt").click(function(){
+/* this if else to change the text in the button*/
+      if($(".navbutt").text() == "☰"){
+       $(".navbutt").text("🞬");
+      }else{
+        $(".navbutt").text("☰");
+      }
+    /* this function toggle the visibility of our "li" elements */
+    $("li").toggle("slow");
+  });
+});
