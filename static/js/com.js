7e7303b9080e21ba72ca4a35657405c939a6327a
Abhay Kumar Verma <56693429+darkRaspberry@users.noreply.github.com>
2021-05-17 15:00:08 +0530

    Bumb CSS+JS & fixes

--- static/js/com.js
+++ static/js/com.js
@@ -2,4 +2,5 @@
 var height = (window.innerHeight > 0) ? window.innerHeight : screen.Height;if(height<300){alert("Device is too small to work correctly. Please maintain least height of 300px");console.log("Height test working.");}
 /*Width Test*/
 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;if((width<300)){alert("Device is too small to work correctly. Please maintain least width of 250px");console.log("Width test Working");}
-/*Output as Width & Height*/console.log(width);console.log(height);
+/*Output as Width & Height*/
+console.log(width);console.log(height);
